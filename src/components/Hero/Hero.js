1dcd4f035fcff586bdbe4aeb6fbdad7dba84651d
Scott Slagle <spsbiz@hotmail.com>
2026-04-08 17:21:37 -0700

    Completed animations on hero page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import './Hero.css';
-
+import React, { useState } from "react";
+import "./Hero.css";
 
 export default function Hero() {
+  const [hoverState, setHoverState] = useState("center");
+
   return (
     <div className="hero">
       <div className="top-nav">
         <div className="logo">
-          SKINSTRIC <span className="bracket">[</span> INTRO <span className="bracket">]</span>
+          SKINSTRIC <span className="bracket">[</span> INTRO{" "}
+          <span className="bracket">]</span>
         </div>
-        <button className="enter-code-btn">
-          ENTER CODE
-        </button>
+        <button className="enter-code-btn">ENTER CODE</button>
       </div>
 
-      <div className="side-square left-square">
+      <div
+        className={`side-square left-square ${hoverState === "right" ? "hidden" : ""}`}
+      >
         <div className="square-inner">
           <div className="square-border"></div>
         </div>
-        <button className="side-button left-button">
+        <button
+          className="side-button left-button"
+          onMouseEnter={() => setHoverState("left")}
+          onMouseLeave={() => setHoverState("center")}
+        >
           <div className="button-diamond">
             <span className="button-arrow">◀</span>
           </div>
@@ -26,11 +32,17 @@ export default function Hero() {
         </button>
       </div>
 
-      <div className="side-square right-square">
+      <div
+        className={`side-square right-square ${hoverState === "left" ? "hidden" : ""}`}
+      >
         <div className="square-inner">
           <div className="square-border"></div>
         </div>
-        <button className="side-button right-button">
+        <button
+          className="side-button right-button"
+          onMouseEnter={() => setHoverState("right")}
+          onMouseLeave={() => setHoverState("center")}
+        >
           <span className="button-text">TAKE TEST</span>
           <div className="button-diamond">
             <span className="button-arrow">▶</span>
@@ -38,16 +50,20 @@ export default function Hero() {
         </button>
       </div>
 
-      <div className="center-content">
-        <h1 className="main-title">
-          Sophisticated<br />skincare
-        </h1>
-      </div>
+      <div className={`text-container ${hoverState}`}>
+  <div className="title-wrapper">
+    <h1 className="main-title">
+      <span className="title-inner">Sophisticated<br />skincare</span>
+    </h1>
+  </div>
+</div>
 
       <div className="bottom-text">
         <p>
-          SKINSTRIC DEVELOPED AN A.I. THAT CREATES<br />
-          A HIGHLY-PERSONALISED ROUTINE TAILORED TO<br />
+          SKINSTRIC DEVELOPED AN A.I. THAT CREATES
+          <br />
+          A HIGHLY-PERSONALISED ROUTINE TAILORED TO
+          <br />
           WHAT YOUR SKIN NEEDS.
         </p>
       </div>
